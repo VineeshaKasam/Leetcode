7f302186524689c801e2820fb8dacc89849123e4
Vineesha Kasam <vkasam@asu.edu>
2018-08-26 15:05:47 -0700

    valid parentheses

---
+++ valid_parentheses.py
@@ -0,0 +1,31 @@
+'''
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+'''
+
+def isValid(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+
+    stack = []
+    dict = {"]": "[", "}": "{", ")": "("}
+    for char in s:
+        if char in dict.values():
+            stack.append(char)
+        elif char in dict.keys():
+            if stack == [] or dict[char] != stack.pop():
+                return False
+        else:
+            return False
+    return stack == []
+
+print(isValid('{}[]()'))
+print(isValid('{[])'))
