46ab18613ac18128fde1319745c26f58d5e6909e
Vineesha Kasam <vkasam@asu.edu>
2018-08-29 22:56:47 -0700

    adding one to a list of digits as integer

---
+++ plusOne.py
@@ -0,0 +1,15 @@
+'''
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+'''
+
+def plusOne(digits):
+    theint = int("".join([str(i) for i in digits])) + 1
+
+    new_list = []
+    for k in range(0, len(str(theint))):
+        new_list.append(int(str(theint)[k]))
+
+    return new_list
+
+print(plusOne([4,3,2]))
+print(plusOne([0]))
