536b87fc9235bcc4886da9bdf04f00d1c635eb4f
Vineesha Kasam <vkasam@asu.edu>
2018-09-14 12:59:11 -0700

    Added columns titles for MS Excel sheets

---
+++ Excel_sheet_columns.py
@@ -0,0 +1,9 @@
+'''
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+'''
+
+def convertToTitle(n):
+
+    return "" if n == 0 else convertToTitle((n - 1) / 26) + chr((n - 1) % 26 + ord('A'))
+
+print convertToTitle(701)
