759bdafeb442da71d3c0153f88c99f229304aa43
Vineesha Kasam <vkasam@asu.edu>
2018-09-27 11:04:36 -0700

    adding the strings

---
+++ addStrings.py
@@ -0,0 +1,8 @@
+'''
+Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+'''
+def addStrings(num1, num2):
+
+    return str(int(num1) + int(num2))
+
+print addStrings("45","67")
