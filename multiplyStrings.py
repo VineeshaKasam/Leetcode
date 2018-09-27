e41819a8e5a4a10882f3ad3a2696258b5de67d2e
Vineesha Kasam <vkasam@asu.edu>
2018-09-27 11:02:23 -0700

    multiplication of two strings

---
+++ multiplyStrings.py
@@ -0,0 +1,9 @@
+'''
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+'''
+
+def multiply(num1, num2):
+
+    return str(int(num1) * int(num2))
+
+print multiply("23","45")
