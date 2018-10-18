2bbc44ad04012c3593fca95d6a11243973a8f221
Vineesha Kasam <vkasam@asu.edu>
2018-10-18 11:58:14 -0700

    constructing binary tree from preorder and inorder traversals

---
+++ constructBinaryTree1.py
@@ -0,0 +1,29 @@
+'''
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if inorder:
+            idx = inorder.index(preorder.pop(0))
+
+            root = TreeNode(inorder[idx])
+
+            root.left = self.buildTree(preorder, inorder[0:idx])
+            root.right = self.buildTree(preorder, inorder[idx + 1:])
+
+            return root
